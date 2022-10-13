a93d708030334b453c5d29501b2aa9f23d243124
meetkool <lcs2021023@iiitl.ac.in>
2022-10-13 06:12:08 +0530

    hacktoberfest /Glitch-Sidder

--- javaScriptFun/Glitch-Sidder/script.js
+++ javaScriptFun/Glitch-Sidder/script.js
@@ -1,12 +1,10 @@
 // set images
 var images = [
-	 "https://bloximages.chicago2.vip.townnews.com/mankatofreepress.com/content/tncms/assets/v3/editorial/5/8e/58e483b4-6881-11e5-8f30-0718665e18a4/560da24a24bd1.image.jpg?crop=1387%2C780%2C0%2C331&resize=1387%2C780&order=crop%2Cresize",
-	
-	"http://hmongouachon.com/_demos/rgbShiftSlider/02-portrait-large.jpg",
-	
-	"http://hmongouachon.com/_demos/rgbShiftSlider/04-portrait-large.jpg",
-	
-	"http://hmongouachon.com/_demos/rgbShiftSlider/05-portrait-large.jpg",
+	"/images/kool2.jpg",
+	"/images/kool1.jpg",
+	"/images/kool3.webp",
+	"/images/kool4.jpg",
+	"/images/kool6.jpg",
   ]
   // instanciate slider
   var rbgShiftSlider = new rbgShiftSlider({
