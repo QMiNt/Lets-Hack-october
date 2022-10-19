3c43207a05bb8a197f86c919fd78322624f0b6ef
Shreyaaaarauniyar <114307725+Shreyaaaarauniyar@users.noreply.github.com>
2022-10-19 19:36:13 +0530

    initial commit

---
+++ DSAcodes/C/array/arraybubblesort.c
@@ -0,0 +1,40 @@
+//bubble sort in an array (shreya rauniyar)
+#include <stdio.h>
+ 
+void change(int* x, int* y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+ 
+
+void bubbleSort(int arr[], int n)
+{
+    
+    for (int i = 0; i < n - 1; i++)
+ 
+        
+        for (int j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j + 1])
+                change(&arr[j], &arr[j + 1]);
+}
+ 
+
+void printArray(int arr[], int size)
+{
+    
+    for (int i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+ 
+int main()
+{
+    int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    bubbleSort(arr, n);
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    return 0;
+}
