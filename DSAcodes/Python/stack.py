52e86dbc58378c6b0daab21384988423579e9d80
lakshay singhal <91775547+plakshay@users.noreply.github.com>
2022-10-21 10:57:45 +0530

    stack.py (#58)
    * initial commit
    
    * added lltraversal in python
    
    * Create insertatend.py
    
    * Create stack.py

---
+++ DSAcodes/Python/stack.py
@@ -0,0 +1,20 @@
+
+ 
+s = []
+
+s.append('a')
+s.append('b')
+s.append('c')
+ 
+print('Initial stack')
+print(s)
+ 
+
+print('\nElements popped from stack:')
+print(s.pop())
+print(s.pop())
+print(s.pop())
+ 
+print('\nStack after elements are popped:')
+print(s)
+ 
